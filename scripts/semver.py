d145e550143886310a1978a09b6d56efe2968602
Karwoo Tang <karwoo.tang@gmail.com>
2020-04-25 13:16:18 +0100

    semver.py is wip

--- scripts/semver.py
+++ scripts/semver.py
@@ -2,83 +2,97 @@
 
 import json
 import sys
+import argparse
 
-if len(sys.argv) > 1:
-  branch = sys.argv[1]
-  branch = branch.lower()
-else:
-  branch = "patch"
+class Semver():
 
-# expected branch prefixes: major/*, feature/*, patch/*, hotfix/*.
-# Everything else will be deemed a patch
-
-release_type = "minor"
-if branch.startswith("feature/"):
-    release_type = "minor"
-elif branch.startswith("hotfix/"):
-    release_type = "patch"
-elif branch.startswith("patch/"):
-    release_type = "patch"
-elif branch.startswith("major/"):
-    release_type = "major"
-else:
-    release_type = "patch"
-
-
-default_version_data = \
+    def __init__(self, filename = "version.json"):
+        self.default_version_data = \
 """
 {
-	"version": { "major": 0, "minor": 0, "patch": 0 }
+    "version": { "major": 0, "minor": 0, "patch": 0 }
 }
 """
+        # supported types
+        self.types = ['major', 'minor', 'patch']
+        self.filename = filename
 
-# supported types
-types = ['major', 'minor', 'patch']
+        self.version = None
+        try:
+            with open(self.filename, "r") as fh:
+                self.version = json.load(fh)
+        except FileNotFoundError:
+            self.version = json.loads(self.default_version_data)
+        except IOError:
+            print("IO error, don't know how to deal with this!")
+            exit(1)
 
+    # expected branch prefixes: major/*, feature/*, patch/*, hotfix/*.
+    # Everything else will be deemed a patch
 
-def get_version(release_type_):
-	if release_type_ not in types:
-		print("Invalid release type: {}!".format(release_type_))
-		exit(1)
-	try:
-		with open("version.json", "r") as fh:
-			version = json.load(fh)
-	except FileNotFoundError:
-		#print("Version file not found, starting from v1.0.0!")
-		version = json.loads(default_version_data)
-		#print(version)
-	except IOError:
-		print("IO error, don't know how to deal with this!")
-		exit(1)
+    def get_next_version(self, release_type="patch"):
+        if release_type not in self.types:
+            print("Invalid release type: {}!".format(release_type))
+            exit(1)
 
-	if release_type.lower() == "patch":
-		version['version']['patch'] = version['version']['patch'] + 1
-	elif release_type.lower() == "minor":
-		version['version']['minor'] = version['version']['minor'] + 1
-		# Patch version MUST be reset to 0 when minor version is incremented.
-		version['version']['patch'] = 0
-	elif release_type.lower() == "major":
-		version['version']['major'] = version['version']['major'] + 1
-		# Minor and patch version MUST be reset to 0 when major version is incremented.
-		version['version']['minor'] = 0
-		version['version']['patch'] = 0
+        if release_type.lower() == "patch":
+            self.version['version']['patch'] = self.version['version']['patch'] + 1
+        elif release_type.lower() == "minor":
+            self.version['version']['minor'] = self.version['version']['minor'] + 1
+            # Patch version MUST be reset to 0 when minor version is incremented.
+            self.version['version']['patch'] = 0
+        elif release_type.lower() == "major":
+            self.version['version']['major'] = self.version['version']['major'] + 1
+            # Minor and patch version MUST be reset to 0 when major version is incremented.
+            self.version['version']['minor'] = 0
+            self.version['version']['patch'] = 0
+        return self.version
 
-	# write the version file
-	try:
-		with open("version.json", "w") as fh:
-			json.dump(version, fh)
-	except IOError:
-		print("IO error, don't know how to deal with this!")
-		exit(1)
-	return version
+    def commit(self):
+        # write the version file
+        try:
+            with open(self.filename, "w") as fh:
+                json.dump(self.version, fh)
+        except IOError:
+            print("IO error, don't know how to deal with this!")
+            exit(1)
 
+    def get_next_version_from_branch(self, branch="patch/dummy", commit_count=0):
+        release_type = "minor"
+        if branch.startswith("feature/"):
+            release_type = "minor"
+        elif branch.startswith("hotfix/"):
+            release_type = "patch"
+        elif branch.startswith("patch/"):
+            release_type = "patch"
+        elif branch.startswith("major/"):
+            release_type = "major"
+        else:
+            release_type = "patch"
+        return self.get_next_version(release_type)
 
-def as_string(version_):
-	return "{0}.{1}.{2}".format(
-		version_['version']['major'],
-		version_['version']['minor'],
-		version_['version']['patch']
-	)
+    def as_string(self):
+        return "{0}.{1}.{2}".format(
+            self.version['version']['major'],
+            self.version['version']['minor'],
+            self.version['version']['patch']
+        )
 
-v = get_version(release_type_=release_type)
-print("{0}".format(as_string(version_=v)))
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-b', '--branch_name', metavar='<branch_name>', 
+        type=str, required=True, 
+        help="The branch name.")
+    ap.add_argument('-c', '--commit_count', metavar='<commit_count>', 
+        type=int, required=False, default=0,
+        help="Number of commit in the current branch.") 
+    args = ap.parse_args()
+    #print(f"Using branch: {args.branch_name}")
+    #print(f"Number of commits: {args.commit_count}")
+    # The type of args.commit is boolean
+    #print(f"commit: {args.commit}")
+
+    sv = Semver("version.json")
+    v = sv.get_next_version_from_branch(branch=args.branch_name, commit_count=args.commit_count)
+    sv.commit()
+    print("{0}".format(sv.as_string()))
