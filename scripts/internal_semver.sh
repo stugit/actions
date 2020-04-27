fee01f56fd7722d93adbca989d40db490db117db
ktang <karwoo.tang@gmail.com>
2020-04-27 23:46:53 +0100

    Update internal_semver.sh

--- scripts/internal_semver.sh
+++ scripts/internal_semver.sh
@@ -11,9 +11,9 @@ dryrun=${DRY_RUN:-false}
 # KT - add tag_prefix
 tag_prefix=${TAG_PREFIX:-internal-}
 
-git branch contains "$(git log --format="%H" -n 1 | tail - 1)"
-git branch contains "$(git log --format="%H" -n 2 | tail - 1)"
-git branch contains "$(git log --format="%H" -n 3 | tail - 1)"
+git branch contains "$(git log --format=\"%H\" -n 1 | tail - 1)"
+git branch contains "$(git log --format=\"%H\" -n 2 | tail - 1)"
+git branch contains "$(git log --format=\"%H\" -n 3 | tail - 1)"
 
 cd ${GITHUB_WORKSPACE}/${source}
 
